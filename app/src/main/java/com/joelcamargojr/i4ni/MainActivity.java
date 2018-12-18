98ae3205a540efe0da5ec0b7ddc6984e01817740
Joel Camargo <joelc1225@gmail.com>
2018-12-17 21:30:44 -0500

    First Android commit. Add button in layout file

---
+++ app/src/main/java/com/joelcamargojr/i4ni/MainActivity.java
@@ -0,0 +1,13 @@
+package com.joelcamargojr.i4ni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
